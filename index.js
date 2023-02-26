1be7594a24496df9237bbe8aefe22a9764045db9
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-02-25 22:02:57 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function temp(item, index, array) {
+    console.log(item, item.result);
+
+    if(item.result === "W"){
+
+        return true;
+    }
+    else {
+
+        return false;
+    }
+
+}
+
+function superbowlWin (record){
+
+    const year = record.find(temp);
+    if(year === undefined){
+        return year;
+    }
+    return year.year;
+}
